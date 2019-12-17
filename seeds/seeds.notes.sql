4044eff6a4fb3533e3f43fd7f87919b2a1375dd8
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2019-12-17 14:15:40 -0500

    update

---
+++ seeds/seeds.notes.sql
@@ -0,0 +1,11 @@
+INSERT INTO notes (note_name, content)
+VALUES
+    (
+        'Note 1', 'This is the first note.'
+    )
+    (
+        'First note', 'This is actually the second note.'
+    )
+    (
+        'Note Alpha', 'More like Omega if you ask me.'
+    )
